2bb8a1b55d28a4884fbf3c99b80855483c9ea660
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-11-15 15:43:06 +0100

    [Research] Extract external target api into custom publication

--- libraries/tools/kotlin-gradle-plugin/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin/build.gradle.kts
@@ -12,6 +12,40 @@ plugins {
 apply(from = "functionalTest.gradle.kts")
 val functionalTestImplementation by configurations
 
+/*
+External Target API
+ */
+run {
+    val mainCompilation = kotlin.target.compilations.getByName("main")
+
+    val externalTargetApiMainCompilation = kotlin.target.compilations.create("externalTargetApiMain") {
+        associateWith(mainCompilation)
+    }
+
+    val externalTargetApiElements = configurations.create("externalTargetApiElements") {
+        isCanBeResolved = false
+        isCanBeConsumed = true
+    }
+
+    artifacts.add(externalTargetApiElements.name, externalTargetApiMainCompilation.compileKotlinTaskProvider)
+
+    val externalTargetApiCompilationJar = tasks.create<Jar>("externalTargetApiJar") {
+        from(externalTargetApiMainCompilation.compileKotlinTaskProvider)
+    }
+
+    configurations.getByName("externalTargetApiMainImplementation")
+        .extendsFrom(configurations.implementation.get())
+
+    publishing {
+        publications.create<MavenPublication>("externalTargetApi") {
+            artifactId = "kotlin-gradle-external-target-api"
+            artifact(externalTargetApiCompilationJar) {
+                builtBy(externalTargetApiCompilationJar)
+            }
+        }
+    }
+}
+
 configure<GradlePluginDevelopmentExtension> {
     isAutomatedPublishing = false
 }
